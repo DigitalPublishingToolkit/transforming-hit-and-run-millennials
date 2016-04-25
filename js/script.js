f1635e31bd52fa9327c9b9da0c572af6fe1cd9df
Lucia Dossin <lucia@fiveblackcats.com>
2016-04-25 16:49:59 +0200

    tabbed content added

--- js/script.js
+++ js/script.js
@@ -16,4 +16,29 @@ $(document).ready(function(){
 
   //still not saving the position of the items, just allowing the user to play around with the interface for now.
 
+  //hide all divs except #article
+  $("#explore").hide();
+  $("#discussion").hide();
+
+  //define variables that we're going to use later
+  var curContent;
+  var curTab;
+
+  $('.pagetabs a').on('click', function(blaEvent){
+    //remove .selected from all li
+    $('.pagetabs li').removeClass('selected');
+    //add .selected to selected tab
+    curTab = $(this).parent();
+    $(curTab).addClass('selected');
+    //defines the current content tab
+    curContent = $(this).attr('href');
+    //hides all itesm with class .tabContent
+    $('.tabContent').hide();
+    //shows current content tab
+    $(curContent).show();
+    //disables page 'scroll' to anchor tag
+    blaEvent.preventDefault();
+    //alert('clicked on '+bla.target.nodeName);
+  });
+
 });//end document.ready

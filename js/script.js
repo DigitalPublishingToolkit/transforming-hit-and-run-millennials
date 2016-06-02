db0e7bde19e565da4324d97a6e764625fad4590b
Lucia Dossin <lucia@fiveblackcats.com>
2016-06-02 13:57:44 +0200

    added message page to the article text - needs styling and content

--- js/script.js
+++ js/script.js
@@ -150,7 +150,7 @@ $('.subheaderExDutch').on('click', function() {
   if($('#caret').hasClass('fa-caret-left')){
     $('#caret').removeClass('fa-caret-left').addClass('fa-caret-down');
   } else{
-    $('#caret').removeClass('fa-caret-down').addClass('fa-caret-left'); 
+    $('#caret').removeClass('fa-caret-down').addClass('fa-caret-left');
   }
 
 });
@@ -163,7 +163,7 @@ $('.subheaderExWorld').on('click', function() {
   if($('#caret').hasClass('fa-caret-left')){
     $('#caret').removeClass('fa-caret-left').addClass('fa-caret-down');
   } else{
-    $('#caret').removeClass('fa-caret-down').addClass('fa-caret-left'); 
+    $('#caret').removeClass('fa-caret-down').addClass('fa-caret-left');
   }
 
 });
@@ -287,9 +287,19 @@ if($('#paginated-content').length){ //if the div with the id 'paginated content'
         //start off with no page text
         var pageText = null;
         for(var i = 0; i < words.length; i++) {
-            //add the next word to the pageText
+            //add the next word to the pageText (this will be done word by word)
+            //if pageText exists, add a space (' ') to it and the next word (word[i])
+            //else just add the next word (words[i])
+            //Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/Operators/Conditional_Operator
+            //equivalent to
+            // if(pageText){
+            //   betterPageText = pageText + ' ' + words[i];
+            // }else{
+            //   betterPageText = words[i]
+            // }
             var betterPageText = pageText ? pageText + ' ' + words[i]
                                           : words[i];
+            //add betterPageText (defined in the line above) as content of newPage
             newPage.html(betterPageText);
 
             //Check if the page is too long
@@ -308,6 +318,10 @@ if($('#paginated-content').length){ //if the div with the id 'paginated content'
                 pageText = betterPageText;
             }
         }
+        //add 'MessagePage' after the second page ( .eq(1) )
+        $('.articleTextPage').eq(1).after('<div id="MessagePage" class="articleTextPage" />');
+        //define a height to this div
+        $('#MessagePage').css('height', pageHeight+'px');
     }
 
     //$(window).resize(paginate).resize();
@@ -324,6 +338,7 @@ if($('#paginated-content').length){ //if the div with the id 'paginated content'
 if($('#paginated-content').length){
 
     var numPages = $('#paginated-content').children().length;
+
     function pilePages(){
 
       for(var n=0; n < numPages; n++){
